d0cdcba10a543d8d2fa14b34e042794a68d15d86
Tom Zhang <zhangtom54321@gmail.com>
2016-03-28 10:08:05 -0700

    Created SetUp.java

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,6 +1,16 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class WelcomeScreen {
+public class WelcomeScreen implements ActionListener{
+	private JButton play;
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == play){
+			
+		}
+	}
 	
 }
