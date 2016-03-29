0b40c9118653af9edd9154b9f25b12c0bdddd487
Tej Qu Nair <tej.q.nair@gmail.com>
2016-03-29 10:00:46 -0700

    Organize imports in WelcomeScreen

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,12 +1,12 @@
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
 
 public class WelcomeScreen implements ActionListener{
 	private JButton play;
 	
+	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == play){
 			
