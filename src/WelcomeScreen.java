159a334cfdaca0fbdc888ee6aed1432497675678
Tej Qu Nair <tej.q.nair@gmail.com>
2016-04-06 10:59:59 -0700

    Fixed WelcomeScreen to not use two panels (Tom)

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -11,25 +11,26 @@ public class WelcomeScreen extends JPanel implements ActionListener {
 
 	private JButton play;
 	private JButton howToPlay;
-	private static JPanel welcomePanel;
 	private SpringLayout layout;
 	
 	public WelcomeScreen(){
-		welcomePanel = new JPanel();
 		layout = new SpringLayout();
-		welcomePanel.setLayout(layout);
+		setLayout(layout);
 		
 		play = new JButton("Play");
 		play.addActionListener(this);
-		welcomePanel.add(play);
+		add(play);
 		
 		howToPlay = new JButton("How to Play");
 		howToPlay.addActionListener(this);
-		welcomePanel.add(howToPlay);
+		add(howToPlay);
 		
-		layout.putConstraint(SpringLayout.WEST, play, 20, SpringLayout.WEST, welcomePanel);
-	    layout.putConstraint(SpringLayout.NORTH, play, 20, SpringLayout.NORTH, welcomePanel);
-	    layout.putConstraint(SpringLayout.NORTH, howToPlay, 20, SpringLayout.NORTH, welcomePanel);
+		layout.putConstraint(SpringLayout.WEST, play, 20, SpringLayout.WEST,
+				this);
+		layout.putConstraint(SpringLayout.NORTH, play, 20, SpringLayout.NORTH,
+				this);
+		layout.putConstraint(SpringLayout.NORTH, howToPlay, 20,
+				SpringLayout.NORTH, this);
 	    layout.putConstraint(SpringLayout.WEST, howToPlay, 20, SpringLayout.EAST, play);
 	}
 	
