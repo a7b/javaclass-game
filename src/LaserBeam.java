e8df674b9e55bcf4505917a695b4f8b76e5a258f
Tom Zhang <zhangtom54321@gmail.com>
2016-04-08 09:45:27 -0700

    Updated method in LaserBeam.java

--- src/LaserBeam.java
+++ src/LaserBeam.java
@@ -1,14 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+import javax.swing.Timer;
 
 
-public class LaserBeam {
+public class LaserBeam implements ActionListener {
 	private AffineTransform laserTransform;
 	private WorldObject laser;
+	private Timer tm;
 	
-	LaserBeam(int vectorx, int vectory, int anchorx, int anchory){
+	LaserBeam(int radians){
 		laserTransform = new AffineTransform();
-		laser = new WorldObject(new Rectangle2D.Double(30, 40, 10, 500), laserTransform);
-		laserTransform.rotate(vectorx, vectory, anchorx, anchory);
+		laser = new WorldObject(new Rectangle2D.Double(640, 0, 10, 1280), laserTransform);
+		laserTransform.rotate(radians);
+		tm = new Timer(1500, this);
+		tm.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == tm){
+		}
 	}
 }
